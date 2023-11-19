95f2987c1fa5c71be242ef7eccc99dfd71d78fc9
SebastienAuberger <sebastien?abg@gmail.com>
2023-11-19 02:03:14 +0100

    Add Music Timeline + one more song

--- player.js
+++ player.js
@@ -28,8 +28,7 @@ const previous_button = document.getElementById("player_previous")
 const music_artist = document.getElementById("music_artist")
 const music_cover = document.getElementById("music_cover")
 const music_title = document.getElementById("music_title")
-const music_timeline = document.getElementById("music_timeline")
-const music_timeline_background = document.getElementById("music_timeline_background")
+
 
 let is_music_playing = false
 let currentMusic = 0
@@ -45,18 +44,6 @@ play_button.addEventListener("click", () => {
         music.pause()
         is_music_playing = false
     }
-})
-
-music.addEventListener("timeupdate", () => {
-    percentage = music.currentTime * 100 / music.duration
-    music_timeline.style.width = percentage + "%"
-})
-
-music_timeline_background.addEventListener("click",(e) => {
-    var bcr = music_timeline_background.getBoundingClientRect()
-    width_percentage = (Math.min(Math.max(0,(e.clientX - bcr.left) / bcr.width),1)) * 100
-    console.log(width_percentage)
-    music.currentTime = music.duration * width_percentage / 100 
 })
 
 next_button.addEventListener("click", () => {
